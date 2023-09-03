e7fe19fb3188ef0591c5edb42dce45e9f409fb71
JoeCT_Chang <JoeCT.Chang@moxa.com>
2023-09-03 14:41:25 +0800

    chore: add storylite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import storylitePlugin from "@storylite/vite-plugin";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    storylitePlugin({
+      stories: "stories/**/*.stories.tsx", // relative to process.cwd()
+    }),
+    react(),
+  ],
+});
