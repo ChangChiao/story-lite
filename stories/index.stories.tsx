bb0dfe227e7fa110cdd8cebb5b4312f63ca29f41
JoeCT_Chang <JoeCT.Chang@moxa.com>
2023-09-04 20:23:00 +0800

    feat: add story

---
+++ stories/index.stories.tsx
@@ -0,0 +1,15 @@
+import { Story } from "@storylite/storylite";
+
+import "../src/styles/components.css";
+
+export default {
+  title: "Welcome",
+  navigation: {
+    icon: <span>🏠</span>,
+    order: 0, // put on top
+    hidden: false, // dont show the default export component in the navigation
+  },
+  component: () => (
+    <div>This is a default component defined in the default export.</div>
+  ),
+} satisfies Story;
